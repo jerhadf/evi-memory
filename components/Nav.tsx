e85b8d87c5e3e45d1f33d3d962d6c5e3e07998b7
jerhadf <jhadfield129@gmail.com>
2024-11-15 15:31:46 -0500

    Fix github link

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -41,7 +41,11 @@ export const Nav = () => {
       <div className={"flex items-center gap-1"}>
         <Button
           onClick={() => {
-            window.open(pkg.homepage, "_blank", "noopener noreferrer");
+            window.open(
+              "https://github.com/jerhadf/evi-memory",
+              "_blank",
+              "noopener noreferrer"
+            );
           }}
           variant={"ghost"}
           className={"ml-auto flex items-center gap-1.5"}
